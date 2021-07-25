765ece68311b988e8beda279a43efc04ea09dbe5
jakeogh <github.com@v6y.net>
2021-07-25 12:15:41 -0700

    auto-commit

--- icecream/icecream.py
+++ icecream/icecream.py
@@ -107,7 +107,7 @@ def build_call_path(outer_frames):
     retry_on_exception_section = False
     item = None
     for index, item in enumerate(call_list_reversed):
-    #    eprint(index, item)
+        eprint(index, item)
         if index > 0:
             if item['path'].startswith('click/'):
                 if not click_section:
