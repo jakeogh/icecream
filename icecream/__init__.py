425e9fa362415cded7d2451d2dbbcbe90b298531
jakeogh <github.com@v6y.net>
2022-10-02 19:29:59 -0700

    auto-commit

--- icecream/__init__.py
+++ icecream/__init__.py
@@ -10,13 +10,15 @@
 # License: MIT
 #
 
-from os.path import dirname, join as pjoin
+from os.path import dirname
+from os.path import join as pjoin
 
+from .builtins import install
+from .builtins import uninstall
 from .icecream import *  # noqa
-from .builtins import install, uninstall
 
 # Import all variables in __version__.py without explicit imports.
 meta = {}
-with open(pjoin(dirname(__file__), '__version__.py')) as f:
+with open(pjoin(dirname(__file__), "__version__.py")) as f:
     exec(f.read(), meta)
 globals().update(dict((k, v) for k, v in meta.items() if k not in globals()))
