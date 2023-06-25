ca539b43795dcd587f5916b2edf31e911c9a33de
jakeogh <github.com@v6y.net>
2023-06-24 19:48:35 -0700

    auto-commit

--- icecream/icecream.py
+++ icecream/icecream.py
@@ -138,6 +138,7 @@ def build_call_path(outer_frames):
                     call_path.append(("<AT>"))
                 asserttool_section = True
                 continue
+            eprint(item["path"])
 
             click_section = False
             retry_on_exception_section = False
